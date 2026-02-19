82f147bd4c8a808bd0498d37a842154c6907cd25
Anshuman1313 <168716686+Anshuman1313@users.noreply.github.com>
2026-02-19 12:35:56 +0530

    BasicFoundationForHeroSection
    BasicFoundationForHeroSection still some font  styling left but we are 80% there

--- src/components/CakeComponents/Hero.tsx
+++ src/components/CakeComponents/Hero.tsx
@@ -1,36 +1,57 @@
 import React from 'react'
+import { Cta, Cta2 } from './Cta'
+import { Hamburger } from 'lucide-react'
+import Hamburger1 from '../MotionDev/Hamburger'
 
 const Hero = () => {
     return (
         <div className='h-screen bg-gray-100 w-full '>
-            <div className="h-full w-full p-3 "> 
-                <div className="hero-bg relative size-full rounded-2xl overflow-clip">
-                    <div className="absolute top-0 h-full w-full flex flex-col ">
-                        <div className="w-full h-10 flex justify-center  text-background mt-4">
-                        <h1 className='font-milkshake  text-6xl max-md:text-4xl  '>DreamSliceStudio</h1>
-                        </div>
-                        
-                        <div className='h-full flex  p-2  w-full'>
+            <div className="w-full z-50 mt-4 px-8 absolute  flex gap-40 justify-between  items-center  text-background ">
+                <div className='w-full flex items-center gap-2'>
+                     <span className='size-17 bg-background rounded-full flex justify-center items-center'> <Hamburger1/></span>
+                     <span className='text-2xl '>Menu</span>
+                     </div>
+                <h1 className='font-milkshake shrink-0 text-center  text-6xl max-md:text-4xl text-heading-cake '>Dream Slice Studio</h1>
+                <div className='w-full text-heading-cake flex justify-end'><Cta2 /></div>
 
-                            <h1 className='h-60 w-[60%] text-8xl font-cooper '>Baked with Love, Crafted for Dreams</h1>
-                            <h1></h1>
+            </div>
+
+            <div className="h-full w-full p-3 ">
+                <div className="hero-bg relative size-full rounded-2xl overflow-clip">
+                    <div className="absolute top-0 h-full w-full flex flex-col z-10">
+
+                        <div className='h-full    flex-col justify-end flex px-4 py-3   w-full'>
+                            <div className='flex gap-30'>
+                                <h1 className='  font-bold w-[60%]  text-8xl font-cooper text-white'>Baked with<br /> Love,<br /> Crafted for<br /> Dreams</h1>
+                                <div className='font-semibold text-background font-cooper  flex flex-col justify-center text-4xl'>
+                                    <span>Every slice made fresh</span>
+                                    <span>to turn your sweetest </span>
+                                    <span>moments into memories.</span>
+                                    <div className='font-cooper text-heading-cake font-semibold py-2'>
+                                        <Cta />
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
+                        {/* <div><Cta /></div>
+                        <div><Cta2 /></div> */}
                     </div>
 
-                 <video src="/cake/CCL2.mp4" 
-                autoPlay
-                muted
-                loop
-                playsInline
-                preload="metadata"
-                className='size-full object-cover'>
+                    <video src="/cake/CCL2.mp4"
+                        autoPlay
+                        muted
+                        loop
+                        playsInline
+                        preload="metadata"
+                        className='size-full object-cover z-0 brightness-80'>
 
-                </video>
-                
+                    </video>
+
                 </div>
-                </div>
-            
-          
+            </div>
+
+
 
 
         </div>
